0699b259f59978265732a463c4ff87f06d75af9b
Lafi84 <Lafi84@gmail.com>
2018-08-22 22:58:50 +0300

    Tehtävä 1.5
    Välipalautus 1.5

--- Osa 1/app1_1/src/index.js
+++ Osa 1/app1_1/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 const Otsikko = (props) => {
     return(
         <div>
-            <h1>{props.name}</h1>
+            <h1>{props.course.nimi}</h1>
         </div>
     )
 };
@@ -20,9 +20,9 @@ const Osa = (props) => {
 const Sisalto = (props) => {
     return (
         <div>
-            <Osa part={props.osat[0].nimi} task={props.osat[0].tehtavia}/>
-            <Osa part={props.osat[1].nimi} task={props.osat[1].tehtavia}/>
-            <Osa part={props.osat[2].nimi} task={props.osat[2].tehtavia}/>
+            <Osa part={props.course.osat[0].nimi} task={props.course.osat[0].tehtavia}/>
+            <Osa part={props.course.osat[1].nimi} task={props.course.osat[1].tehtavia}/>
+            <Osa part={props.course.osat[2].nimi} task={props.course.osat[2].tehtavia}/>
         </div>
     );
 };
@@ -30,34 +30,36 @@ const Sisalto = (props) => {
 const Yhteensa = (props) => {
     return (
         <div>
-            <p>Yhteensä {props.osat[0].tehtavia
-        +props.osat[1].tehtavia+props.osat[2].tehtavia} tehtävää</p>
+            <p>Yhteensä {props.course.osat[0].tehtavia
+        +props.course.osat[1].tehtavia+props.course.osat[2].tehtavia} tehtävää</p>
         </div>
     )
 };
 
 const App = () => {
-    const kurssi = 'Half Stack -sovelluskehitys';
-    const osat = [
-        {
-            nimi: 'Reactin perusteet',
-            tehtavia: 10
-        },
-        {
-            nimi: 'Tiedonvälitys propseilla',
-            tehtavia: 7
-        },
-        {
-            nimi: 'Komponenttien tila',
-            tehtavia: 14
-        }
-    ];
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+            {
+                nimi: 'Reactin perusteet',
+                tehtavia: 10
+            },
+            {
+                nimi: 'Tiedonvälitys propseilla',
+                tehtavia: 7
+            },
+            {
+                nimi: 'Komponenttien tila',
+                tehtavia: 14
+            }
+        ]
+    }
 
     return (
         <div>
-            <Otsikko name={kurssi}/>
-            <Sisalto osat={osat}/>
-            <Yhteensa osat={osat}/>
+            <Otsikko course={kurssi}/>
+            <Sisalto course={kurssi}/>
+            <Yhteensa course={kurssi}/>
         </div>
     )
 };
