0eabcae940318191189ad18318afc2322091bba1
Lafi84 <Lafi84@gmail.com>
2019-06-28 10:43:26 +0300

    Additional tasks done

--- Osa 2/puhelinluettelo/src/App.js
+++ Osa 2/puhelinluettelo/src/App.js
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Filter from './components/Filter.js'
 import PersonForm from './components/PersonForm.js'
 import Contacts from './components/Contacts.js'
+import contactService from './services/ContactService.js';
+import Notification from "./components/Notification";
 
 const App = () => {
+    const [ successMessage, setSuccessMessage ] = useState(null);
+    const [ errorMessage, setErrorMessage ] = useState(null);
     const [ persons, setPersons] = useState([]);
     const [ newName, setNewName ] = useState('');
     const [ newNumber, setNewNumber ] = useState('');
     const [ filter, setFilter ] = useState('');
 
     useEffect(() => {
-        axios
-            .get('http://localhost:3001/persons')
+        contactService.getAll()
             .then(response => {
-                console.log('promise fulfilled');
-                setPersons(response.data)
+                console.log('Contacts fetched');
+                setPersons(response)
             });
     }, []);
 
@@ -29,19 +31,76 @@ const App = () => {
       }
 
       const personExists = persons.filter(person => person.name === newName);
-      if(personExists.length>0)
-          window.alert(`${newName} is already added to phonebook`);
-      else {
-          const newPerson = {name: newName, number: newNumber};
-          setPersons(persons.concat(newPerson));
+
+      if(personExists.length>0) {
+          const personNumberChange = {...personExists[0], number: newNumber};
+          replaceContact(personNumberChange);
           clearForm();
-      }
+      }else {
+          const newPerson = {name: newName, number: newNumber};
+          contactService.create(newPerson).then(response => {
+              setPersons(persons.concat(response));
+              showSuccessMessage(`Created person ${newPerson.name}`);
+              clearForm();
+          });
+        }
+    };
+
+    const deleteContact = (personToDelete) => {
+        return () => {
+          if(window.confirm(`Do you want to delete contact information from ${personToDelete.name} ?`)){
+              contactService.del(personToDelete.id).then(response => {
+                  setPersons(persons.filter(person => person.id !== personToDelete.id));
+                  showSuccessMessage(`Deleted person ${personToDelete.name}`);
+              });
+          }
+        };
+    };
+
+    const replaceContact = (personToReplace) => {
+        if(window.confirm(`Do you want to replace contact for ${personToReplace.name} ?`)){
+            contactService.update(personToReplace.id, personToReplace).then(response => {
+                setPersons(persons.map(person => person.id === response.id ? response : person));
+                showSuccessMessage(`Replaced phone number for ${response.name}`);
+            }).catch(error => {
+                setPersons(persons.filter(person => person.id !== personToReplace.id));
+                showErrorMessage(`Person could not be modified, person ${personToReplace.name} has already been deleted`);
+            });
+        }
+    };
+
+    const showSuccessMessage = (message) => {
+        setSuccessMessage(message);
+
+        setTimeout(() => {
+            setSuccessMessage(null)
+        }, 5000)
+    };
+
+    const showErrorMessage = (message) => {
+        setErrorMessage(message);
+
+        setTimeout(() => {
+            setErrorMessage(null)
+        }, 5000)
     };
 
     const clearForm = () => {
         setNewName('');
         setNewNumber('');
         setFilter('');
+    };
+
+    const getSuccessMessage = () => {
+        console.log('message', successMessage);
+        if(successMessage!==null)
+          return <Notification message={successMessage} className={'success'}/>
+    };
+
+    const getErrorMessage = () => {
+        console.log('errorMessage', errorMessage);
+        if(errorMessage!==null)
+            return <Notification message={errorMessage} className={'error'}/>
     };
 
     const nameChange = (event) => {
@@ -61,7 +120,9 @@ const App = () => {
             <h1>Phonebook</h1>
             <Filter filter={filter} onChange={filterChange}/>
             <PersonForm submitContact={submitContact} newName={newName} newNumber={newNumber} nameChange={nameChange} numberChange={numberChange} />
-            <Contacts persons={persons} filter={filter}/>
+            {getSuccessMessage()}
+            {getErrorMessage()}
+            <Contacts persons={persons} filter={filter} deleteTrigger={deleteContact}/>
         </div>
     )
 
--- Osa 2/puhelinluettelo/src/components/Contacts.js
+++ Osa 2/puhelinluettelo/src/components/Contacts.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Contacts = ({persons, filter}) => {
+const Contacts = ({persons, filter, deleteTrigger}) => {
     console.log(persons);
     const getContacts = () => {
-        return persons.filter(person => person.name.toLowerCase().indexOf(filter.toLowerCase())>=0).map(person => <div key={person.name}>{person.name} {person.number}</div>);
+        return persons.filter(person => person.name.toLowerCase().indexOf(filter.toLowerCase())>=0).map(person =>
+            <div key={person.name}>{person.name} {person.number} <button onClick={deleteTrigger(person)}>delete</button></div>);
     };
     return (
         <div>
