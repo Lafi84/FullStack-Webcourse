7e1ce7f4c6945198cc21c37e01e8918c988f7f60
Lafi84 <Lafi84@gmail.com>
2019-07-23 11:56:19 +0300

    ESLint

--- Osa 8/book-client/src/components/Authors.js
+++ Osa 8/book-client/src/components/Authors.js
@@ -1,37 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const Authors = (props) => {
-  if (!props.show) {
-    return null
-  }
-  const authors = []
+	if (!props.show) {
+		return null;
+	}
+	const authors = [];
 
-  return (
-    <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
-          </tr>
-          {authors.map(a =>
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+	return (
+		<div>
+			<h2>authors</h2>
+			<table>
+				<tbody>
+					<tr>
+						<th></th>
+						<th>
+                          born
+						</th>
+						<th>
+                          books
+						</th>
+					</tr>
+					{authors.map(a =>
+						<tr key={a.name}>
+							<td>{a.name}</td>
+							<td>{a.born}</td>
+							<td>{a.bookCount}</td>
+						</tr>
+					)}
+				</tbody>
+			</table>
 
-    </div>
-  )
-}
+		</div>
+	);
+};
 
-export default Authors
+export default Authors;
